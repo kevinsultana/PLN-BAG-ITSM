1d4e63a3533dcf802139029e60d35b0402cc2737
kevinsultana <kevinpdg.ks@gmail.com>
2025-07-17 09:39:39 +0800

    fix: vercel suspense useparams

---
+++ src/app/helpdesk/details/DetailsContent.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export default function DetailContent() {
+  const searchParams = useSearchParams();
+  const kode = searchParams.get("kode");
+  const deskripsi = searchParams.get("deskripsi");
+  const requester = searchParams.get("requester");
+  const tanggal = searchParams.get("tanggal");
+  const status = searchParams.get("status");
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Detail Tiket</h1>
+      <p>
+        <strong>Kode:</strong> {kode}
+      </p>
+      <p>
+        <strong>Deskripsi:</strong> {deskripsi}
+      </p>
+      <p>
+        <strong>Requester:</strong> {requester}
+      </p>
+      <p>
+        <strong>Tanggal:</strong> {tanggal}
+      </p>
+      <p>
+        <strong>Status:</strong> {status}
+      </p>
+    </div>
+  );
+}
--- src/app/helpdesk/details/page.jsx
+++ src/app/helpdesk/details/page.jsx
@@ -1,36 +1,10 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
+import DetailContent from "./DetailsContent";
 
 export default function DetailPage() {
-  const searchParams = useSearchParams();
-  const kode = searchParams.get("kode");
-  const deskripsi = searchParams.get("deskripsi");
-  const requester = searchParams.get("requester");
-  const tanggal = searchParams.get("tanggal");
-  const status = searchParams.get("status");
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="p-4">
-        <h1 className="text-xl font-bold">Detail Tiket</h1>
-        <p>
-          <strong>Kode:</strong> {kode}
-        </p>
-        <p>
-          <strong>Deskripsi:</strong> {deskripsi}
-        </p>
-        <p>
-          <strong>Requester:</strong> {requester}
-        </p>
-        <p>
-          <strong>Tanggal:</strong> {tanggal}
-        </p>
-        <p>
-          <strong>Status:</strong> {status}
-        </p>
-      </div>
+    <Suspense fallback={<div>Memuat detail tiket...</div>}>
+      <DetailContent />
     </Suspense>
   );
 }
