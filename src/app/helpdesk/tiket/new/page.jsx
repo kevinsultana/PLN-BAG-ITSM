19b6a9925bcc0b820f1687d74ca0c1b2ce85fcd2
kevinsultana <kevinpdg.ks@gmail.com>
2025-08-14 14:45:28 +0700

    feat: added create new ticket helpdesk side

--- src/app/helpdesk/tiket/new/page.jsx
+++ src/app/helpdesk/tiket/new/page.jsx
@@ -1,11 +1,13 @@
 import HelpdeskLayout from "@/components/Helpdesk/layout/HelpdeskLayout";
+import CreateTicketForm from "@/components/Helpdesk/Tiket/CreateTicketForm";
 import React from "react";
 
 export default function Page() {
   return (
     <div className="bg-slate-100 h-full">
       <HelpdeskLayout>
-        <h1 className="text-2xl font-bold mb-6">Semua Tiket</h1>
+        <h1 className="text-2xl font-bold mb-6">Buat Tiket Baru</h1>
+        <CreateTicketForm />
       </HelpdeskLayout>
     </div>
   );
