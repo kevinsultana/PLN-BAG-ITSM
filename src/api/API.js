090a0eaf88865b11b68da896c24f294793563636
kevinsultana <kevinpdg.ks@gmail.com>
2025-09-25 21:55:43 +0700

    fix: form download pdf

--- src/api/API.js
+++ src/api/API.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:8080";
-// export const BACKEND_URL = "https://itsm-helpdesk-be.unotek.co.id";
+// export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://itsm-helpdesk-be.unotek.co.id";
