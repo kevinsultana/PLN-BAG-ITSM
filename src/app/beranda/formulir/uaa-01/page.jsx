75d097c87b3480666c5a4241dae3397569201d18
kevinsultana <kevinpdg.ks@gmail.com>
2025-10-01 23:25:00 +0700

    fix: adjusting margin on beranda pahse 2

--- src/app/beranda/formulir/uaa-01/page.jsx
+++ src/app/beranda/formulir/uaa-01/page.jsx
@@ -246,7 +246,6 @@ export default function Page() {
           style={{
             display: "flex",
             flexDirection: "column",
-            padding: "1.5rem 3.5rem",
           }}
         >
           <h1
